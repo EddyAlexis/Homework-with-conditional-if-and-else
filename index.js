3a43c6fd6f2df9619da5f4306366dc6719dd4923
EddyAlexis <134143077+EddyAlexis@users.noreply.github.com>
2023-10-15 19:52:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function sauver(){
+    let nom = document.getElementById("name").value
+    let email = document.getElementById("email").value
+    let age = document.getElementById("age").value
+    let type = document.formulaire.type.value
+
+    if(nom ==""){
+        alert("vous devez informer le nom")
+    }
+    if(email ==""){
+        alert("vous devez informer l'email ")
+    }
+    if(age ==""){
+        alert("vous devez informer l'age ")
+    }
+    if(type==""){
+        alert("vous devez informer le type ")
+    }
+    else{
+        alert("vous avez sauvegarde vos informations")
+    }
+        
+    }
